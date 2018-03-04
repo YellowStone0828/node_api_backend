5e774c89b989ad27c5efbadd20e98fa27fcb2b26
YellowStone0828 <sunsiyuan1220@live.cn>
2018-03-05 00:16:26 +0800

    '搭建骨架'

---
+++ src/controllers/api.js
@@ -0,0 +1,41 @@
+export let Get = (ctx) => {
+  ctx.body = {
+    result: 'get',
+    name: ctx.params.name,
+    para: ctx.query
+  }
+}
+
+export let Post = async (ctx) => {
+  ctx.body = {
+    result: 'post',
+    name: ctx.params.name,
+    para: ctx.request.body
+  }
+}
+
+export let Put = (ctx) => {
+  ctx.body = {
+    result: 'put',
+    name: ctx.params.name,
+    para: ctx.request.body
+  }
+}
+
+export let Delect = (ctx) => {
+  ctx.body = {
+    result: 'delect',
+    name: ctx.params.name,
+    para: ctx.request.body
+  }
+}
+
+export let checkwinners = (ctx) => {
+  const play = require('../services/gambling')
+  play.checkwinner(ctx)
+}
+
+export let  testDB = (ctx) => {
+  const play = require('../services/gambling')
+  play.testDB(ctx)
+}
---
+++ src/services/gambling.js
@@ -0,0 +1,54 @@
+
+export let checkwinner = (ctx) =>{
+    let currentUser = ctx.request.body.user
+    //console.log('currentUser',currentUser)
+    if(currentUser&&currentUser.userName=='xin_liang'){
+        ctx.body = {
+            'result':'恭喜！您中奖了'
+        }
+    }else if(currentUser&&currentUser.userName!='xin_liang'){
+        ctx.body = {
+            'result':'真遗憾！感谢您的参与'
+        }    
+    }else{
+        ctx.body = {
+            'result':'相关信息缺失！请检查'
+        }
+    }
+}
+
+import sequelize from '../lib/sequelize'
+import Sequelize from 'sequelize'
+export let testDB = (ctx) =>{
+    //console.log('sequelize',sequelize.query())
+    const User = sequelize.define('user',{
+    id: {
+        type: Sequelize.STRING,
+        field: 'user_id',
+        primaryKey: true 
+        },
+    name: {
+      type: Sequelize.STRING,
+      field: 'user_name'
+    },
+    password: {
+      type: Sequelize.STRING,
+      field: 'user_pwd'
+    }},{
+        timestamps: false,
+        tableName: 'ys_user_info',
+        freezeTableName: true
+      });
+
+    User.findAll({
+        attributes: ['id', 'name'],
+        where:{
+           user_name:ctx.request.body.user.userName 
+        }
+    }).then(user=>{
+        console.log(user[0].dataValues);
+    })
+    // sequelize.query("SELECT * FROM ys_user_info").then(myTableRows => {
+    //      console.log(myTableRows)
+    // })
+}
